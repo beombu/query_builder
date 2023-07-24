04034f2d592fadf3c17dad9edfea4437ecc50216
beombu <beombu13@gmail.com>
2023-07-25 04:41:30 +0900

    SelectBuilder 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,14 @@ package org.example;
 
 import org.example.queryBuilder.conditionBuilder.OrderBuilder;
 import org.example.queryBuilder.conditionBuilder.WhereBuilder;
+import org.example.queryBuilder.constant.Column;
+import org.example.queryBuilder.constant.Table;
+import org.example.queryBuilder.dmlBuilder.SelectBuilder;
 
+import static org.example.queryBuilder.constant.Column.*;
 import static org.example.queryBuilder.constant.Column.CUSTOMER_ID;
 import static org.example.queryBuilder.constant.Column.CUSTOMER_NAME;
+import static org.example.queryBuilder.constant.Table.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,5 +28,13 @@ public class Main {
                 .build();
 
         System.out.println(order.toString());
+
+        SelectBuilder select = new SelectBuilder()
+                .select(ALL)
+                .from(CUSTOMER)
+                .where(where)
+                .build();
+
+        System.out.println(select.toString());
     }
 }
--- src/main/java/org/example/queryBuilder/constant/Column.java
+++ src/main/java/org/example/queryBuilder/constant/Column.java
@@ -2,7 +2,8 @@ package org.example.queryBuilder.constant;
 
 public enum Column {
     CUSTOMER_ID("customer_id"),
-    CUSTOMER_NAME("customer_name")
+    CUSTOMER_NAME("customer_name"),
+    ALL("*")
     ;
 
     private final String column;
