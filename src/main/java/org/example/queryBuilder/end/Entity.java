deec2d5030d0a1d4258cc4f7720350fb56654301
beombu <beombu13@gmail.com>
2023-08-09 06:26:59 +0900

    :sparkles: Select 빌더 구현

---
+++ src/main/java/org/example/queryBuilder/end/Entity.java
@@ -0,0 +1,4 @@
+package org.example.queryBuilder.end;
+
+public interface Entity {
+}
---
+++ src/main/java/org/example/queryBuilder/end/SimpleUser.java
@@ -0,0 +1,19 @@
+package org.example.queryBuilder.end;
+
+public class SimpleUser {
+	String name;
+	int age;
+
+	public SimpleUser(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
